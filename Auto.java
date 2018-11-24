9e92e5e822b53f888d05a4776a00584d2e1ea352
Mary0412 <33613702+Mary0412@users.noreply.github.com>
2018-11-23 21:18:01 -0600

    Add files via upload

---
+++ Auto.java
@@ -0,0 +1,15 @@
+import  java.util.Random.*;
+public class Auto{
+int numero;//para saber que numeo de auto creado es si el 1 o 20 o el 50
+String color, modelo;
+String placas;
+public Auto(String modelo,int numero,String color){
+	this.modelo=modelo;
+	this.numero=numero;
+	this.color=color;
+	
+}
+private void setPlaca(){
+	
+}
+}
