a833103eb6cc7ca57c02d402fa59b2434508983e
ombrenda13 <36515687+ombrenda13@users.noreply.github.com>
2018-11-25 16:20:20 -0600

    Update Nodo.java

--- Nodo.java
+++ Nodo.java
@@ -1,24 +1,22 @@
+
+
+package examen_sistemas_operativos;
+
 public class Nodo {
-    
-    
-    Auto carrito;
+    Auto carro;
+    String color,marca,placa;
     Nodo nodo;
     Nodo anterior;
     Nodo siguiente; //enlaza los nodos (simplemente enlazados)
     Nodo cabeza; //(apunta al final de la lista para ir agregando los nodos) 
     
-   public Nodo(Auto carr){
-		carrito=carr;
+    
+   public Nodo(String color,String marca,String placa){
+		this.color=color;
+                this.marca=marca;
+                this.placa=placa;
 		this.siguiente=null;
                 
 		}
-    
-//   public  void setIdproceso(int id_proceso){
-//		this.id_proceso=id_proceso;
-//                this.siguiente=null; //cuando regrese nulo es que no hay otro nodo enlazado 
-//    }
-   
-    
-    
-    
+
 }
