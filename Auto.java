7a21b552f14f2eb105a06b4d6a630868e3a4e16d
Mary0412 <33613702+Mary0412@users.noreply.github.com>
2018-11-24 19:29:36 -0600

    Update Auto.java

--- Auto.java
+++ Auto.java
@@ -1,15 +1,16 @@
 import  java.util.Random.*;
 public class Auto{
-int numero;//para saber que numeo de auto creado es si el 1 o 20 o el 50
-String color, modelo;
-String placas;
-public Auto(String modelo,int numero,String color){
-	this.modelo=modelo;
-	this.numero=numero;
-	this.color=color;
+	int numero;//para saber que numeo de auto creado es si el 1 o 20 o el 50
+	String color, modelo;
+	String placas;
+	Auto anterior,siguiente;
+	public Auto(String modelo,int numero,String color){
+		this.modelo=modelo;
+		this.numero=numero;
+		this.color=color;
 	
-}
-private void setPlaca(){
+	}
+	private void setPlaca(){
 	
-}
+	}
 }
