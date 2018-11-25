1302669b6dd356b6d03659b2b9fd3241a96b3025
Mary0412 <33613702+Mary0412@users.noreply.github.com>
2018-11-25 09:59:45 -0600

    Update CProductor.java

--- CProductor.java
+++ CProductor.java
@@ -1,18 +1,24 @@
 class CProductor extends Thread {
 		
-		private Auto []entradauto //último mensaje producido
-		public CProductor (Auto c) {
-		entradauto=c;
+		private ListaCar listacar; //último mensaje producido
+		String modelo,color;
+		public CProductor () {
+		
+		}
+		public void setCar(){
+			this.modelo="Nissan";
+			this.color="azul";
 		}
 		public void run() {
 
 		while (true) {
-			nMsj = (int)(Math.random()*100); //genera num. de msj
-		mensaje.Almacenar(nMsj); //almaena el mensaje
-		System.out.println("Productor "+ getName() + "almacena el mensaje #" + nMsj);
-		/*try {sleep(2);
+			synchronized(listacar){
+				Auto car=Auto(marca,color);
+				listacar.agregarFinal(car);
+				System.out.println("Productor "+ getName() + "almacena el mensaje #" + nMsj);
+				try {sleep(2);
 			//sleep((int)(Math.random()*100));
-		} catch (InterruptedException e) {}*/
-		}
+				} catch (InterruptedException e) {}
+			}
 		}
 }
