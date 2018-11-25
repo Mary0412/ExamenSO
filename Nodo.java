de29e025293c9271d3890977c923d986c596324e
Mary0412 <33613702+Mary0412@users.noreply.github.com>
2018-11-25 10:00:57 -0600

    Update Nodo.java

--- Nodo.java
+++ Nodo.java
@@ -1,1 +1,24 @@
-
+public class Nodo {
+    
+    
+    Auto carrito;
+    Nodo nodo;
+    Nodo anterior;
+    Nodo siguiente; //enlaza los nodos (simplemente enlazados)
+    Nodo cabeza; //(apunta al final de la lista para ir agregando los nodos) 
+    
+   public Nodo(Auto carr){
+		carrito=carr;
+		this.siguiente=null;
+                
+		}
+    
+//   public  void setIdproceso(int id_proceso){
+//		this.id_proceso=id_proceso;
+//                this.siguiente=null; //cuando regrese nulo es que no hay otro nodo enlazado 
+//    }
+   
+    
+    
+    
+}
