95188cfe03b193180bd6b8a0b549a6c47ae0a408
ombrenda13 <36515687+ombrenda13@users.noreply.github.com>
2018-11-25 16:20:49 -0600

    Update mainPC.java

--- mainPC.java
+++ mainPC.java
@@ -1,13 +1,32 @@
-
 package examen_sistemas_operativos;
+
+import java.util.*;
 
 public class Examen_sistemas_operativos {
 
     public static void main(String[] args) {
+        
+        int entradas;
+        int salidas;
+        Scanner num_entradas=new Scanner(System.in);
+        Scanner num_salidas=new Scanner(System.in);
+        Lista lista_coches=new Lista();
        Auto auto=new Auto();
        Auto auto2=new Auto();
-        System.out.println("color1:"+auto.color_auto+"\tmarca:"+auto.marca_auto+"\tplacas:"+auto.placa);
-        System.out.println("color2:"+auto2.color_auto+"\tmarca:"+auto2.marca_auto+"\tplacas:"+auto2.placa);
+//        System.out.println("color1:"+auto.color_auto+"\tmarca:"+auto.marca_auto+"\tplacas:"+auto.placa);
+//        System.out.println("color2:"+auto2.color_auto+"\tmarca:"+auto2.marca_auto+"\tplacas:"+auto2.placa);
+//        
+////        System.out.println("ingrese el numero de entradas del estacionanmiento:");
+////        entradas=num_entradas.nextInt();
+////        System.out.println("entradas:"+entradas);
+////        
+////         System.out.println("ingrese el numero de salidas del estacionanmiento:");
+////        salidas=num_salidas.nextInt();
+////        System.out.println("salidas:"+salidas);
+        
+       lista_coches.agregarFinal(auto.color_auto,auto.marca_auto,auto.placa);
+       lista_coches.agregarFinal(auto2.color_auto,auto2.marca_auto,auto2.placa);
+       lista_coches.mostrarFila();
     }
     
 }
