f68b5f23dca1db5c81896bb0c3bad67f7177e609
ombrenda13 <36515687+ombrenda13@users.noreply.github.com>
2018-11-25 12:09:25 -0600

    Update mainPC.java

--- mainPC.java
+++ mainPC.java
@@ -1,9 +1,13 @@
-class mainPC{
-public static void main(String[] args){
-	ListaCar ent1=new ListaCar();
-		CProductor entrada1=new Cproductor();
-   		 CProductor entrada2=new CProductor();
-	}
 
+package examen_sistemas_operativos;
 
+public class Examen_sistemas_operativos {
+
+    public static void main(String[] args) {
+       Auto auto=new Auto();
+       Auto auto2=new Auto();
+        System.out.println("color1:"+auto.color_auto+"\tmarca:"+auto.marca_auto+"\tplacas:"+auto.placa);
+        System.out.println("color2:"+auto2.color_auto+"\tmarca:"+auto2.marca_auto+"\tplacas:"+auto2.placa);
+    }
+    
 }
