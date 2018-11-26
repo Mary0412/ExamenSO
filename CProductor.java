80711577fb305911747e00658da8094d012d892f
Mary0412 <33613702+Mary0412@users.noreply.github.com>
2018-11-25 21:20:51 -0600

    Update CProductor.java

--- CProductor.java
+++ CProductor.java
@@ -1,24 +1,29 @@
 class CProductor extends Thread {
-		
-		private ListaCar listacar; //último mensaje producido
+	private ListaCar listacar; //último mensaje producido
 		String modelo,color;
-		public CProductor () {
-		
+		public CProductor (ListaCar carritos) {
+			listacar=carritos;
 		}
-		public void setCar(){
+		/*public void setCar(){
 			this.modelo="Nissan";
 			this.color="azul";
-		}
+		}*/
 		public void run() {
-
-		while (true) {
-			synchronized(listacar){
-				Auto car=Auto(marca,color);
-				listacar.agregarFinal(car);
-				System.out.println("Productor "+ getName() + "almacena el mensaje #" + nMsj);
+			int i=1;
+		while (i<=5) {
+			//synchronized(listacar){
+				
+				Auto car=new Auto();
+       			listacar.agregarFinal(car.color_auto,car.marca_auto,car.placa);
+				
+				System.out.println("Productor "+ getName() + " almacena el carro #" + i+"\ncolor:"+car.color_auto+"\tmarca:"+car.marca_auto+"\tplaca:"+car.placa);
 				try {sleep(2);
 			//sleep((int)(Math.random()*100));
 				} catch (InterruptedException e) {}
+				i++;
 			}
-		}
+			/*System.out.println("Entrada 1:");
+			listacar.mostrarFila();*/
+		}	
+	
 }
