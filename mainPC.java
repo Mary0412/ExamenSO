0320e199bdbe6e8ef1d631c8503bfc88e2a719f4
Mary0412 <33613702+Mary0412@users.noreply.github.com>
2018-11-25 21:18:29 -0600

    Update mainPC.java

--- mainPC.java
+++ mainPC.java
@@ -8,10 +8,11 @@ public class Examen_sistemas_operativos {
         
         int entradas;
         int salidas;
-        Scanner num_entradas=new Scanner(System.in);
-        Scanner num_salidas=new Scanner(System.in);
-        Lista lista_coches=new Lista();
-       Auto auto=new Auto();
+       // Scanner num_entradas=new Scanner(System.in);
+        //Scanner num_salidas=new Scanner(System.in);
+        ListaCar lista_coches=new ListaCar();
+        CProductor productor1 = new CProductor(lista_coches);
+       /*Auto auto=new Auto();
        Auto auto2=new Auto();
 //        System.out.println("color1:"+auto.color_auto+"\tmarca:"+auto.marca_auto+"\tplacas:"+auto.placa);
 //        System.out.println("color2:"+auto2.color_auto+"\tmarca:"+auto2.marca_auto+"\tplacas:"+auto2.placa);
@@ -27,6 +28,20 @@ public class Examen_sistemas_operativos {
        lista_coches.agregarFinal(auto.color_auto,auto.marca_auto,auto.placa);
        lista_coches.agregarFinal(auto2.color_auto,auto2.marca_auto,auto2.placa);
        lista_coches.mostrarFila();
+       System.out.println("Agregando mas");
+       for(int i=0;i<4;i++){
+       	Auto car=new Auto();
+       	lista_coches.agregarFinal(car.color_auto,car.marca_auto,car.placa);
+      
+       }*/
+       	productor1.start();
+       	try{
+			
+			productor1.join();
+		}catch(InterruptedException ex){}
+       	System.out.println("\n\nEn la entrada 1: ");
+        lista_coches.mostrarFila();
+
     }
     
 }
