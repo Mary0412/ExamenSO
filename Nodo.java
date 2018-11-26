30a3f2871a22c074ae191c8fb4dd243be957911c
Mary0412 <33613702+Mary0412@users.noreply.github.com>
2018-11-26 09:16:27 -0600

    Update Nodo.java

--- Nodo.java
+++ Nodo.java
@@ -3,20 +3,22 @@
 package examen_sistemas_operativos;
 
 public class Nodo {
-    Auto carro;
+    
     String color,marca,placa;
+    Auto coche;
     Nodo nodo;
     Nodo anterior;
     Nodo siguiente; //enlaza los nodos (simplemente enlazados)
     Nodo cabeza; //(apunta al final de la lista para ir agregando los nodos) 
     
     
-   public Nodo(String color,String marca,String placa){
-		this.color=color;
+   public Nodo(Auto c){
+        /*this.color=color;
                 this.marca=marca;
-                this.placa=placa;
-		this.siguiente=null;
+                this.placa=placa;*/
+                coche=c;
+        this.siguiente=null;
                 
-		}
+}
 
 }
